81f73ffaf41dc43ba0b14d90ec27c1ab0525875e
范跃东 <632296950@qq.com>
2020-06-20 15:35:10 +0800

    demo

---
+++ example/src/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "pxtovw-loader",
+            options: {
+              unitToConvert: "px", // 自定义转换单位
+              viewportWidth: 1080, // 视口宽度
+              unitPrecision: 5, // 保留小数位
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
