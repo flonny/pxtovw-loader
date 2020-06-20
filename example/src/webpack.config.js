730d3fc2b287c16d1ab8282e84c81f73f4ee1a98
范跃东 <632296950@qq.com>
2020-06-20 18:27:27 +0800

    更改demo

--- example/src/webpack.config.js
+++ example/src/webpack.config.js
@@ -18,8 +18,9 @@ module.exports = {
             loader: "pxtovw-loader",
             options: {
               unitToConvert: "px", // 自定义转换单位
-              viewportWidth: 1080, // 视口宽度
+              viewportWidth: 600, // 视口宽度
               unitPrecision: 5, // 保留小数位
+              minPixelValue:2
             },
           },
         ],
